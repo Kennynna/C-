7758a9c69b7c12296ad0604b611a276411feb7cb
Kennynna <kennynna@mail.ru>
2023-09-20 11:24:45 +0300

    Exp 4.0

--- Lessons/Exp 5.0 (if)/Program.cs
+++ Lessons/Exp 5.0 (if)/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write("Введите ваше имя ");
 string username = Console.ReadLine();
-if ( username == "Маша"){
+if ( username.ToLower() == "маша"){
     Console.WriteLine("Ура, это же Маша");
 } else{
-    Console.Write("Привет");
+    Console.Write("Привет ");
     Console.WriteLine(username);
 }
