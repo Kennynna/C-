5fd17d2516151069fda85e9bd68ec5b8b3d2ed8b
Kennynna <kennynna@mail.ru>
2023-09-24 21:13:25 +0300

    HomeWork_2

---
+++ Lessons/HomeWork_2/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100 || number > 1000){
+    Console.WriteLine("число должно быть трехзначным! ");
+} else {
+    int number2 = number / 10;
+    number2 = number2 % 10;
+    Console.WriteLine($"вторая цифра из {number} -> {number2} ");
+}
+
+Console.WriteLine(" ");
+Console.WriteLine("Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+if (num >= 100){
+    while (num >=1000){
+        num=num / 10;
+    }
+    num=num % 10; 
+    Console.WriteLine($"  третяя цифра  {num} ");
+} else if (num < -100){
+    num = num * -1;
+     while (num >=1000){
+        num=num / 10;
+    }
+    num=num % 10; 
+    Console.WriteLine($"  третяя цифра  {num} ");
+} else {
+    Console.WriteLine($"нет");
+};
+
+
+Console.WriteLine(" ");
+Console.WriteLine("Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+Console.Write("Введите число ");
+int day = int.Parse(Console.ReadLine());    
+if (day == 1){
+    Console.WriteLine($"нет");
+} else if(day == 2){
+    Console.WriteLine($"нет");
+} else if(day == 3){
+    Console.WriteLine($"нет");
+} else if(day == 4){
+    Console.WriteLine($"нет");
+} else if(day == 5){
+    Console.WriteLine($"нет");
+} else if(day == 6){
+    Console.WriteLine($"да");
+} else if(day == 7){
+    Console.WriteLine($"да");
+} else {
+    Console.WriteLine($"Введите корректный день");
+}
+
+
